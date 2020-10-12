de86878b61249e5f890da6b3d77aaf3c9042e367
mut <gustavo.ceci95@gmail.com>
2020-10-12 02:29:20 -0400

    shaders + better terrain + goblin house

---
+++ Assets/SkyboxModifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxModifier : MonoBehaviour
+{
+  public Material skyboxMaterial;
+
+  public void SetPitch(float pitch)
+  {
+    skyboxMaterial.SetFloat("_Pitch", pitch);
+  }
+
+  public void SetTint(Color color)
+  {
+    skyboxMaterial.SetColor("_Tint", color);
+  }
+
+  public void SetExposure(float exposure)
+  {
+    skyboxMaterial.SetFloat("_Exposure", exposure);
+  }
+}
