5951f47caf4f26b31b0a33ec8852f0d9486961a2
mut <gustavo.ceci95@gmail.com>
2020-10-22 21:58:56 -0400

    added UI hand HandLogic

---
+++ Assets/ChangeSpriteWithString.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ChangeSpriteWithString : MonoBehaviour
+{
+  public Sprite[] sprites;
+  [SerializeField] private bool GetFromResources;
+  [SerializeField] private string PathInResources;
+
+  private Image img;
+
+  void Awake()
+  {
+    img = GetComponent<Image>();
+    if (GetFromResources)
+    {
+      sprites = Resources.LoadAll<Sprite>(PathInResources);
+    }
+  }
+
+  public void SetSpriteByString(string text)
+  {
+    if (img == null) return;
+    var newSprite = sprites.FirstOrDefault(sprite => sprite.name.ToLowerInvariant().Contains(text.ToLowerInvariant()));
+    if (newSprite == null)
+    {
+      Debug.LogWarning($"Could not find {text} portrait");
+      img.enabled = false;
+    }
+    else
+    {
+      img.sprite = newSprite;
+      img.enabled = true;
+    }
+  }
+}
---
+++ Assets/HandLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+public class HandLogic : MonoBehaviour
+{
+  public StringReference LeftHandItem;
+  public StringReference RightHandItem;
+
+  public StringReference EmptyString;
+
+  public void GiveItem(string item)
+  {
+    if (RightHandItem.Value == null || RightHandItem.Value.ToLowerInvariant() == EmptyString.Value.ToLowerInvariant())
+    {
+      RightHandItem.Value = item;
+    }
+
+    else if (LeftHandItem.Value == null || LeftHandItem.Value.ToLowerInvariant() == EmptyString.Value.ToLowerInvariant())
+    {
+      LeftHandItem.Value = item;
+    }
+  }
+
+  public void DropLeftHand()
+  {
+    LeftHandItem.Value = EmptyString.Value;
+  }
+
+  public void DropRightHand()
+  {
+    RightHandItem.Value = EmptyString.Value;
+  }
+}
