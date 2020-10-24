2a2178639bd16774cef591869612899e28cfc902
mut <gustavo.ceci95@gmail.com>
2020-10-24 01:48:25 -0400

    made hunter disappear after conversation

---
+++ Assets/TransformTo2dItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TransformTo2dItem : MonoBehaviour
+{
+  public StringEvent giveItemEvent;
+  public StringReference itemName;
+  public UnityEvent callback;
+  // Start is called before the first frame update
+  void Start()
+  {
+    giveItemEvent.Register((item) =>
+    {
+      if (itemName.Value.ToLowerInvariant() != item.ToLowerInvariant()) return;
+      callback?.Invoke();
+    });
+  }
+}
