b837d6f5d4bdcdd8249189ded4f51583023b79c5
mut <gustavo.ceci95@gmail.com>
2020-10-25 01:14:55 -0400

    3d gifts!

---
+++ Assets/GiveItemToPlayerIfPossible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GiveItemToPlayerIfPossible : MonoBehaviour
+{
+  public StringReference itemName;
+  public BoolReference playerHasEmptySlot;
+  public GameObject itemToDisappear;
+  public UnityEvent callback;
+
+  public StringEvent giveItem;
+
+  public void GiveItemToPlayer()
+  {
+    if (playerHasEmptySlot.Value)
+    {
+      itemToDisappear.SetActive(false);
+      giveItem.Raise(itemName.Value);
+      callback?.Invoke();
+    }
+  }
+}
--- Assets/HandLogic.cs
+++ Assets/HandLogic.cs
@@ -8,19 +8,26 @@ public class HandLogic : MonoBehaviour
   public StringReference LeftHandItem;
   public StringReference RightHandItem;
   public StringReference LastTakenItem;
+  public StringReference LastDroppedItem;
+  public BoolReference GiveItemAttemptSuccessful;
+  public BoolReference HasEmptySlot;
 
   public StringReference EmptyString;
 
   public void GiveItem(string item)
   {
+
+    GiveItemAttemptSuccessful.Value = false;
     if (RightHandItem.Value == null || RightHandItem.Value.ToLowerInvariant() == EmptyString.Value.ToLowerInvariant())
     {
       RightHandItem.Value = item;
+      GiveItemAttemptSuccessful.Value = true;
     }
 
     else if (LeftHandItem.Value == null || LeftHandItem.Value.ToLowerInvariant() == EmptyString.Value.ToLowerInvariant())
     {
       LeftHandItem.Value = item;
+      GiveItemAttemptSuccessful.Value = true;
     }
   }
 
@@ -29,22 +36,31 @@ public class HandLogic : MonoBehaviour
     if (RightHandItem.Value.ToLowerInvariant() == item.ToLowerInvariant())
     {
       LastTakenItem.Value = item;
-      RightHandItem.Value = EmptyString;
+      RightHandItem.Value = EmptyString.Value;
     }
     else if (LeftHandItem.Value.ToLowerInvariant() == item.ToLowerInvariant())
     {
       LastTakenItem.Value = item;
-      LeftHandItem.Value = EmptyString;
+      LeftHandItem.Value = EmptyString.Value;
     }
   }
 
   public void DropLeftHand()
   {
+    LastDroppedItem.Value = LeftHandItem.Value;
     LeftHandItem.Value = EmptyString.Value;
   }
 
   public void DropRightHand()
   {
+    LastDroppedItem.Value = LeftHandItem.Value;
     RightHandItem.Value = EmptyString.Value;
+  }
+
+  private void Update()
+  {
+    HasEmptySlot.Value =
+      RightHandItem.Value == EmptyString.Value.ToLowerInvariant()
+      || LeftHandItem.Value == EmptyString.Value.ToLowerInvariant();
   }
 }
